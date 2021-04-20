538ced28909a17f092d325ea6f6fcc597ecd24be
Hai Trang <s0565011@htw-berlin.de>
2021-04-20 14:51:18 +0200

    changes

--- front/src/app/pages/photos/form/form.component.ts
+++ front/src/app/pages/photos/form/form.component.ts
@@ -1,16 +1,20 @@
 import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from "@angular/forms";
 import { Data } from '../../../shared/data';
+import { BackendService } from "../../../shared/backend.service";
+
+
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
   form: FormGroup;
   @Input() data: Data;
   @Output() updateEvent = new EventEmitter<Data>();
+  private BackendService: BackendService;
 
   constructor(private fb: FormBuilder, private location: Location) {
     this.form = this.fb.group(
@@ -31,14 +35,18 @@ export class FormComponent implements OnInit {
 
   }
 
-  delete(): void {
+  delete(dataId: number): void {
+
+    this.BackendService.deleteOne(dataId);
 
   }
 
-  onUpdate(): void {
+  onUpdate(dataId: number, data: Data): void {
     const values = this.form.value;
     this.data.id = values.idControl;
     this.data.path = values.pathControl;
+
+    this.BackendService.update(this.data.id, this.data);
 
 }
 }
